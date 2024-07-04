d96c53ae1da19b9bc63ebc4244bc8d6487b5b369
vlad0318 <revutskyyvladyslav@gmail.com>
2024-07-03 19:11:56 -0700

    Added a hint into the text imput and added a sperate evets page

--- login.py
+++ login.py
@@ -10,10 +10,15 @@ class LoginScreen(GridLayout):
 
 	def __init__(self,**kwargs):
 		super(LoginScreen,self).__init__(**kwargs)
+		# This sets the amount of columns
 		self.cols = 2
+
+		# This sets the username field
 		self.add_widget(Label(text='username'))
-		self.username = TextInput(multiline=False)
+		self.username = TextInput(multiline=False,hint_text="username")
 		self.add_widget(self.username)
+
+		# This sets the username field
 		self.add_widget(Label(text='password'))
-		self.password = TextInput(password=True,multiline=False)
+		self.password = TextInput(password=True,multiline=False, hint_text="password")
 		self.add_widget(self.password)
